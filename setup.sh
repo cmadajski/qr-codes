c07d2cf45eebcf931c0a349d042158cd258ef31d
cmadajski <cmad7317@tutanota.com>
2023-06-23 17:11:54 -0400

    Fix conditional operator in setup

--- setup.sh
+++ setup.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
+# color codes
 green="\033[32m"
 red="\033[91m"
 orange="\033[93m"
 blue="\033[96m"
 reset="\033[0m"
 
+# important variables
 venv_created=0
 venv_activated=0
 steps_success=0
 steps_total=5
 
 echo "    QR Code Generator - Setup"
+# assume Python is installed by default on all Linux and MacOS systems
 echo -e "${blue}[-]${reset} Python installed."
 steps_success=$(($steps_success + 1))
+# if Python is installed, then ensurepip will install Pip if necessary
 python3 -m ensurepip --upgrade
 echo -e "${blue}[-]${reset} Pip installed."
 steps_success=$(($steps_success + 1))
+
 # create virtual environment
 python3 -m venv .venv
-if [[ -f ".venv" ]]; then
+if [[ -d ".venv" ]]; then
     echo -e "${green}[-]${reset} Virtual environment created."
     steps_success=$(($steps_success + 1))
     venv_created=1
 else
     echo -e "${red}[!]${reset} ERROR while creating virtual environment."
 fi
+
 # activate virtual environment
 if [[ $venv_created -eq 1 ]]; then
     source .venv/bin/activate
@@ -37,6 +43,7 @@ if [[ $venv_created -eq 1 ]]; then
         venv_activated=1
     fi
 fi
+
 # install dependencies
 if [[ $venv_activated -eq 1 ]]; then
     python3 -m pip install -r requirements.txt
@@ -44,6 +51,7 @@ if [[ $venv_activated -eq 1 ]]; then
     steps_success=$(($steps_success + 1))
 fi
 
+# print summary
 if [[ $steps_success -eq 5 ]]; then
     echo -e "    SUMMARY: ${green}${steps_success}${reset} / ${steps_total} successful."
 elif [[ $steps_success -gt 2 ]]; then
