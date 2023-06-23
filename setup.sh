a79ef906824d47740ec17cef36d9519b80f8653a
cmadajski <cmad7317@tutanota.com>
2023-06-23 16:43:08 -0400

    Add conditionals to setup

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ blue="\033[96m"
 reset="\033[0m"
 
 steps_success=0
-steps_total=4
+steps_total=5
 
 echo "    QR Code Generator - Setup"
 echo -e "${blue}[-]${reset} Python installed."
@@ -20,15 +20,34 @@ python3 -m venv .venv
 if [ -f ".venv" ]; then
     echo -e "${green}[-]${reset} Virtual environment created."
     steps_success=$(($steps_success + 1))
+    venv_created=1
 else
     echo -e "${red}[!]${reset} ERROR while creating virtual environment."
+    venv_created=0
 fi
 # activate virtual environment
-source .venv/bin/activate
-if [ -z "$(env | grep )" ]; then
-    echo -e "${red}[!]${reset} ERROR virtual env could not be activated."
-else
-    echo -e "${green}[-]${reset} Virtual env activated."
+if [ $venv_created -eq 1 ]; then
+    source .venv/bin/activate
+    if [ -z "$(env | grep )" ]; then
+        echo -e "${red}[!]${reset} ERROR virtual env could not be activated."
+        venv_activated=0
+    else
+        echo -e "${green}[-]${reset} Virtual env activated."
+        steps_success=$(($steps_success + 1))
+        venv_activated=1
+    fi
+fi
+# install dependencies
+if [ $venv_activated -eq 1 ]; then
+    python3 -m pip install -r requirements.txt
+    echo -e "${green}[-]${reset} Dependencies installed"
     steps_success=$(($steps_success + 1))
 fi
-echo -e "    SUMMARY: ${blue}${steps_success}${reset} / ${steps_total} successful."
+
+if [ $steps_success -eq 5 ]: then
+    echo -e "    SUMMARY: ${green}${steps_success}${reset} / ${steps_total} successful."
+elif [ $steps_success -gt 2 ]; then
+    echo -e "    SUMMARY: ${orange}${steps_success}${reset} / ${steps_total} successful."
+else
+    echo -e "    SUMMARY: ${red}${steps_success}${reset} / ${steps_total} successful."
+fi
